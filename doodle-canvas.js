cc17a590ac50db846139d34ba122a88708438671
Hannah Loop <hannah@manamiami.com>
2018-06-28 20:08:00 -0400

    Changes made to JS

--- doodle-canvas.js
+++ doodle-canvas.js
@@ -1,65 +1,196 @@
-// window.onload = function() {
-// helloooo  
-
-var myCanvas = document.getElementById('doodleJump');
-var ctx = myCanvas.getContext('2d')
-myCanvas
-myCanvas.width = 500;
-myCanvas.height=700;
-  var currentGame;
-  var theImage;
-  // height: 700px;
-  // width: 500px
-
-  //MY PLAYER FUNCTION
-  var chewy = function(x,y,width,height){
-    var ctx = document.getElementById('doodleJump').getContext('2d');
-    var x = x;
-    var y = y;
-    var width = width;
-    var height = height;
-    var img = new Image();
-
-  img.onload = function() {
-    ctx.drawImage(img, x, y,width,height);
+window.onload = function() {
+  document.getElementById("start-button").onclick = function() {
+    startGame();
   };
-  img.src = './images/Chewy.png';
+  function startGame() {
+    myGameArea.myObstacles = [];
+    myGameArea.start();
+    player = new Component(
+      50,
+      50,
+      "./images/Chewy.png",
+      myGameArea.canvas.width / 2 - 15,
+      myGameArea.canvas.height - 100,
+      "player"
+    );
+  }
+  var lines = 0;
+  var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function() {
+      this.canvas.width = 570;
+      this.canvas.height = 700;
+      this.context = this.canvas.getContext("2d");
+      document.getElementById("game-board").append(this.canvas);
+      this.reqAnimation = window.requestAnimationFrame(updateGameArea);
+    },
+    backgroud: function() {},
+    myObstacles: [],
+    frames: 60,
+    clear: function() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function() {
+      cancelAnimationFrame(this.reqAnimation);
+      this.gameOver();
+    },
+    gameOver: function() {
+      this.clear();
+    }
+  };
+
+  // CONTSTRUCTOR FUNCTION TO DRAW OBJECTS (chewy,platform) 
+  function Component(width, height, image, x, y, type) {
+    this.width = width;
+    this.height = height;
+    this.type = type;
+    this.gravity = 1;
+    this.gravitySpeed = 0;
+    this.x = x;
+    this.y = y;
+    this.speedY = 0;
+    this.speedX = 0;
+    
+    
+    this.image = new Image();
+    this.update = function() {
+      ctx = myGameArea.context;
+      if (this.type == "player") {
+        this.image.src = "images/Chewy.png";
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      } else {
+        this.image.src = "images/platform.png";
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      }
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    this.hitBottom = function() {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            this.gravitySpeed = 0;
+        }
+    };
+    
+    this.left = function() {
+      return this.x;
+    };
+    this.right = function() {
+      return this.x + this.width -35;
+    };
+    this.top = function() {
+      return this.y;
+    };
+    this.bottom = function() {
+      return this.y + this.height -5;
+    };
+    this.crashWith = function(obstacle) {
+      return !(
+        player.bottom() < obstacle.top() ||
+        player.top() > obstacle.bottom() ||
+        player.right() < obstacle.left() ||
+        player.left() > obstacle.right()
+      );
+
+    this.platformStep = function () {
+      if(player.bottom >= obstacle.top) {
+        player.x = obstacle.posX;
+        this.hitBottom()
+      }
+    }
+    };
+  }
+
+  // function updateGameArea() {
+  //   for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
+  //     if (player.crashWith(myGameArea.myObstacles[i])) {
+  //       myGameArea.stop();
+  //       startGame();
+  //       return;
+  //     }
+  //   }
+  //   myGameArea.clear();
+  //   myGameArea.backgroud();
+  //   drawObstacles();
+  //   myGameArea.frames += 4;
+  //   for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
+  //     myGameArea.myObstacles[i].y += 1;
+  //     myGameArea.myObstacles[i].update();
+  //   }
+  //   player.update();
+  //   player.newPos();
+  //   myGameArea.reqAnimation = window.requestAnimationFrame(updateGameArea);
+
+  
+  // }
+
+  // FUNCTION THAT DRAWS new platforms
+  function drawObstacles() {
+    if (myGameArea.frames % 110 === 0) {
+      minWidth = (myGameArea.canvas.width - 5) * 0.09;
+      maxWidth = (myGameArea.canvas.width - 5) * 0.2;
+      width = minWidth + Math.floor(Math.random() * ((maxWidth-20) - minWidth));
+      posX =
+        40 + Math.floor(Math.random() * (myGameArea.canvas.width - 80 - width));
+      myGameArea.myObstacles.push(new Component(width, 20, "images/platform.png", posX, 0));
+    }
     
   }
-  //CALLING MY PLAYER FUNCION
+  document.onkeydown = function(e) {
+    if (e.keyCode == 39 && player.x < myGameArea.canvas.width -60 ) {
+      player.x += 30;
+    }
+    if (e.keyCode == 37 && player.x > 0) {
+      player.x -= 30;
+    }
+    if (e.keyCode == 38 ) {
+      player.y -= 150;
+      player.platformStep()
+    }
+  };
 
-chewy(220,643,50,50)
-  // Chewy.prototype.drawChewy = function(){
-  //   var that = this;
-  //   theImage = new Image();
-  //   theImage.src = that.img;
-  //   theImage.onload = function(){
-  //     ctx.drawImage(theImage, that.x, that.y,  that.width, that.height)
-  //   }
-  // }
- 
-  
-  // chewy.prototype.move = function(whichArrow, jump) {
-  //   ctx.clearRect(this.x, this.y, this.width, this.height);
-    
-  //   switch (whichKey){
-  //     case 'ArrowLeft':
-  //     this.x -=5;
-  //     break;
-  //     case 'ArrowRight':
-  //     this.y +=5;
-  //     break;
-  //     case 'ArrowUp':
-  //     this.y -=5;
-  //     break;
-  //     case 'ArrowDown':
-  //     this.y +=5;
+  function updateGameArea() {
+    for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
+      if (player.crashWith(myGameArea.myObstacles[i])) {
+        myGameArea.stop();
+        startGame();
+        return;
+      }
+    }
+    // if (keysDown[K_UP]){
+    //   if (this.falling == false){
+    //   this.setImage("images/Chewy.png");
+    //   this.y -= 5;
+    //   this.falling = true;
+    //   this.addVector(0, 15);
+    // } // end if
+    //   } else {
+    //     checkFalling();
+    //     }// end if
 
-  //   }   
-  //   ctx.drawImage(theImage, this.x, this.y, this.width, this.height);
-  // }
-  
-  
+    myGameArea.clear();
+    myGameArea.backgroud();
+    drawObstacles();
+    myGameArea.frames += 4;
+    for (i = 0; i < myGameArea.myObstacles.length; i += 1) {
+      myGameArea.myObstacles[i].y += 1;
+      myGameArea.myObstacles[i].update();
+    }
+    player.update();
+    player.newPos();
+    myGameArea.reqAnimation = window.requestAnimationFrame(updateGameArea);
+  }
+  var score = 0;
+  function drawScore() {
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("Score: " + score, 600, 20); //need to change score position
+}
+};
 
 
-    
